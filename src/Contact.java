52d086cce12f67b61b4d44ca597119846901e413
Ngô Minh Hiếu <hieunm92@gmail.com>
2021-01-21 09:25:21 +0700

    creat class ManageContacts

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,35 @@
 public class Contact {
+    private int phoneNumber;
+    private String group;
+    private String fullName;
+    private String sex;
+    private String address;
+    private String birthday;
+    private String email;
+
+    public Contact() {
+    }
+
+    public Contact(int phoneNumber, String group, String fullName, String sex, String address, String birthday, String email) {
+        this.phoneNumber = phoneNumber;
+        this.group = group;
+        this.fullName = fullName;
+        this.sex = sex;
+        this.address = address;
+        this.birthday = birthday;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "phoneNumber=" + phoneNumber +
+                ", group='" + group + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", sex='" + sex + '\'' +
+                ", address='" + address + '\'' +
+                ", birthday='" + birthday + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
